JupiterOne/lens

Fix type errors
Signed-off-by: Sebastian Malton <sebastian@malton.name>

src/common/fetch/create-headers.injectable.ts
@@ -1,0 +1,20 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import { getInjectable } from "@ogre-tools/injectable";
+import type { Headers, HeadersInit } from "node-fetch";
+import __UNSAFE_nodeFetchInjectable from "./unsafe-fetch.injectable";
+
+export type CreateHeaders = (init?: HeadersInit) => Headers;
+
+const createHeadersInjectable = getInjectable({
+  id: "create-headers",
+  instantiate: (di): CreateHeaders => {
+    const { Headers } = di.inject(__UNSAFE_nodeFetchInjectable);
+
+    return (init) => new Headers(init);
+  },
+});
+
+export default createHeadersInjectable;

src/common/fetch/fetch.injectable.ts
@@ -4,13 +4,13 @@
  */
 import { getInjectable } from "@ogre-tools/injectable";
 import type { RequestInit, Response } from "node-fetch";
-import __UNSAFE_fetchInjectable from "./unsafe-fetch.injectable";
+import __UNSAFE_nodeFetchInjectable from "./unsafe-fetch.injectable";
 
 export type Fetch = (url: string, init?: RequestInit) => Promise<Response>;
 
 const fetchInjectable = getInjectable({
   id: "fetch",
-  instantiate: (di) => di.inject(__UNSAFE_fetchInjectable),
+  instantiate: (di): Fetch => di.inject(__UNSAFE_nodeFetchInjectable).default,
   causesSideEffects: true,
 });
 

ngeorger/openlens

src/common/fetch/create-headers.injectable.ts
@@ -1,0 +1,20 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import { getInjectable } from "@ogre-tools/injectable";
+import type { Headers, HeadersInit } from "node-fetch";
+import nodeFetchModuleInjectable from "./fetch-module.injectable";
+
+export type CreateHeaders = (init?: HeadersInit) => Headers;
+
+const createHeadersInjectable = getInjectable({
+  id: "create-headers",
+  instantiate: (di): CreateHeaders => {
+    const { Headers } = di.inject(nodeFetchModuleInjectable);
+
+    return (init) => new Headers(init);
+  },
+});
+
+export default createHeadersInjectable;

src/common/fetch/fetch.injectable.ts
@@ -10,11 +10,7 @@ export type Fetch = (url: string, init?: RequestInit) => Promise<Response>;
 
 const fetchInjectable = getInjectable({
   id: "fetch",
-  instantiate: (di): Fetch => {
-    const { default: fetch } = di.inject(nodeFetchModuleInjectable);
-
-    return (url, init) => fetch(url, init);
-  },
+  instantiate: (di): Fetch => di.inject(nodeFetchModuleInjectable).default,
   causesSideEffects: true,
 });
 